26b53120c2afc815b29bcc70cb42e45cc2c5f455
stage <stage@etapes-print.com>
2025-02-24 10:21:34 +0100

    requiremnts.txt and pyproject.toml added

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Bah, c'est une app flask quoi.
+
+#### To make your project portable, you can export the list of installed packages:
+
+## pip freeze > requirements.txt 
+
+### Create a new virtual environment
+python3 -m venv myenv
+source myenv/bin/activate
+### Install dependencies
+pip install -r requirements.txt
+
+I install python on my Ubuntu system. However, when I try to run any python script returns an error. I found a solution on the internet that says I should create a virtual environment (venv). As a dyslexic person that have low latent inhibition, I want to understand what happens when I install python on my machine, how and why it works or doesn't, how do the venv work, and what are the best pratices. Please give me a detailed explanation with concrate examples.
--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask # importing the FLask class.
 
-app = Flask(__name__)
+app = Flask(__name__) # An instance of the class
 
-@app.route("/")
+@app.route("/") # telling FLask what URL should trigger our function
 def hello_world():
-    return "Hello, World!"
+    return "<h1>Hello, World!</h1> <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum lies.</p>"
 
 # 1. python3 -m venv myenv , 2. source myenv/bin/activate , 3. pip install flask , 4. flask --app app run
